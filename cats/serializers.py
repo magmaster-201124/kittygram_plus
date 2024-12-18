4baac06597ee2ad84669ec6b7df6789cfa8cdaa2
Marat Garifulun <magmasters@gmail.com>
2024-12-18 12:05:32 +0700

    kitty_1_5

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,85 @@
-from rest_framework import serializers
+import datetime as dt
 
-from .models import Cat
+from rest_framework import serializers
+import webcolors
+
+from cats.models import Cat, Owner, Achievement, AchievementCat, CHOICES
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # Убрано переопределение owner = serializers.StringRelatedField(read_only=True)
+    # Переопределяем поле achievements
+    achievements = AchievementSerializer(many=True, required=False)  # Убрано read_only=True
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()  # собственный тип поля, описанный выше
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+    def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        # Иначе делаем следующее:
+        # Уберём список достижений из словаря validated_data и сохраним его
+        achievements = validated_data.pop('achievements')
+        # Сначала добавляем котика в  без его достижений
+        cat = Cat.objects.create(**validated_data)
+        # А потом добавляем его достижения в БД или получаем их существующие экземпляры из БД
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+            # И связываем каждое достижение с этим котиком в связной модели
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat)
+        return cat
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
